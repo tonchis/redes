f84c46b0473716b3de2824836528a6ad8c929681
tonchis <lucas.tolchinsky@gmail.com>
2014-09-06 21:17:44 -0300

    Filter who_has only.

--- tp1/sniffer.py
+++ tp1/sniffer.py
@@ -5,8 +5,14 @@ def callback(package):
     arp = package.getlayer(ARP)
     print str(arp.psrc) + " > " + str(arp.pdst)
 
+def who_has(package):
+    if package.haslayer(ARP):
+        arp = package.getlayer(ARP)
+        return arp.op == arp.who_has
+    return False
+
 config.sniff_promisc = True
 
 if __name__ == "__main__":
-    sniff(prn = callback, filter = "arp", store = 0)
+    sniff(prn = callback, lfilter = who_has, store = 0)
 
