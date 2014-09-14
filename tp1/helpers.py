edb1221ee1e3fcec8657da355e8c870cc99af612
tonchis <lucas.tolchinsky@gmail.com>
2014-09-14 18:02:12 -0300

    Rename ARPSources to ARPSample and document some.

--- tp1/helpers.py
+++ tp1/helpers.py
@@ -1,29 +1,51 @@
 import scapy.utils
 import collections
 
-ARPSources = collections.namedtuple("ARPSources", ["src", "dst"])
+# Sample of ARP packages.
+#
+# ARPSample#src: [List] holds all the source ips in the sample.
+# ARPSample#dst: [List] holds all the destination ips in the sample.
+ARPSample = collections.namedtuple("ARPSample", ["src", "dst"])
+
+# Count the occurrances of each ip in an ARPSample.
+#
+# IPOccurrence#src: [Int] the times a given ip was seen in a sample as a source address.
+# IPOccurrence#dst: [Int] the times a given ip was seen in a sample as a destination address.
 IPOccurrence = collections.namedtuple("IPOccurrence", ["src", "dst"])
 
+# Turn a ARP sample pcap file into a Tuple of ips.
+# For each ARP package in the sample, we extract the source and destination ips.
+#
+# pcap_file: [String] path to the ARP pcap file.
+# return: [ARPSample]
 def load_sources(pcap_file):
     arp_packages = scapy.utils.rdpcap(pcap_file)
 
-    arp_sources = ARPSources(src=[], dst=[])
+    arp_sample = ARPSample(src=[], dst=[])
 
     for package in arp_packages:
-        arp_sources.src.append(package.psrc)
-        arp_sources.dst.append(package.pdst)
+        arp_sample.src.append(package.psrc)
+        arp_sample.dst.append(package.pdst)
 
-    return arp_sources
+    return arp_sample
 
-def ips_in_sample(arp_sources):
-    return set(arp_sources.src).union(set(arp_sources.dst))
+# All the ips seen in a given sample, both as source and destination.
+#
+# arp_sample: [ARPSample] the Tuple representing the sample.
+# return: [Set] all the ips are strings.
+def ips_in_sample(arp_sample):
+    return set(arp_sample.src).union(set(arp_sample.dst))
 
-def count_occurrences(arp_sources):
-    ips = ips_in_sample(arp_sources)
+# Count how many times each ip in the sample was seen as source and destination.
+#
+# arp_sample: [ARPSample] the Tuple representing the sample.
+# return: [Dict] keys are ips [String] and values are occurrences [IPOccurrence].
+def count_occurrences(arp_sample):
+    ips = ips_in_sample(arp_sample)
     occurrences = {}
 
     for ip in ips:
-        occurrences[ip] = IPOccurrence(src=arp_sources.src.count(ip), dst=arp_sources.dst.count(ip))
+        occurrences[ip] = IPOccurrence(src=arp_sample.src.count(ip), dst=arp_sample.dst.count(ip))
 
     return occurrences
 
