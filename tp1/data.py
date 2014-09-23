50280f9b0952b9cf92975c2fae629a33517136bb
Nicolas Vallejo <nico_pr08@hotmail.com>
2014-09-23 18:13:39 -0300

    script to plot histograms from arp packages

---
+++ tp1/data.py
@@ -0,0 +1,50 @@
+#!/usr/bin/evn python
+import sys
+import scapy.all
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if len(sys.argv)<1:
+	sys.exit("python tgfpcap.py path/to/pcapfile numberofpackagestoread apariciones")
+
+src = list()
+dst = list()
+
+#read pcap file
+packages = scapy.utils.rdpcap(sys.argv[1], int(sys.argv[2]))
+
+for package in packages:
+	src.append(package.psrc)
+	dst.append(package.pdst)
+	
+ips =[]
+count=[]
+label =""
+if (int(sys.argv[3]) == 0):
+	source = set(src)
+	for s in source:
+		if src.count(s) > int(sys.argv[4]):
+			label = "fuente"
+			ips.append(s)
+			count.append(src.count(s))
+else:
+	destination = set(dst)
+	for d in destination:
+		if dst.count(d) > int(sys.argv[4]):
+			label="destino"
+			ips.append(d)
+			count.append(dst.count(d))
+
+pos = np.arange(len(ips))
+width = 1.0     # gives histogram aspect to the bar diagram
+
+ax = plt.axes()
+ax.set_xticks(pos + (width / 2))
+ax.set_xticklabels(ips, rotation=90, size=7.75)
+
+plt.bar(pos, count, width, color='r')
+plt.title("TITULO - IPs "+ label+ " -  Mas de " +str(int(sys.argv[4])) + " apariciones")
+plt.ylabel('Frecuencia')
+plt.show()
+
