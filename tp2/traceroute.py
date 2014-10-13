52d5fda81eca62aa5e48d1b423077c26622fc016
tonchis <lucas.tolchinsky@gmail.com>
2014-10-13 15:11:38 -0300

    Some more refactoring.

--- tp2/traceroute.py
+++ tp2/traceroute.py
@@ -19,15 +19,15 @@ TIME_EXCEEDED = 11
 
 routers = []
 for ttl in range(1, options.max_ttl + 1):
-    print "TTL: %s" % ttl
+    print "TTL:", ttl
     res = scapy.sendrecv.sr1(scapy.layers.inet.IP(dst=options.url, ttl=ttl) / scapy.layers.inet.ICMP(), timeout=options.timeout, verbose=options.verbose)
     if res:
-        icmp_response = res.getlayer(scapy.layers.inet.ICMP)
-        if icmp_response.type == ECHO_REPLY:
-            routers.append(icmp_response.src)
+        icmp_res = res.getlayer(scapy.layers.inet.ICMP)
+        if icmp_res.type == ECHO_REPLY:
+            routers.append(icmp_res.src)
             break
-        elif icmp_response.type == TIME_EXCEEDED:
-            routers.append(icmp_response.src)
+        elif icmp_res.type == TIME_EXCEEDED:
+            routers.append(icmp_res.src)
 
 print routers
 
